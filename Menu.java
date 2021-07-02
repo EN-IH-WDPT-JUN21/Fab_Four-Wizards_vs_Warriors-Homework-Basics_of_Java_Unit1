776c0c5a9653831e9e70aa5053aeee19aae70c4a
vivi-chaudiem <nguyen.chaudiem@yahoo.de>
2021-07-02 14:39:47 +0200

    Added loop for teamsize for random party

--- Menu.java
+++ Menu.java
@@ -2,19 +2,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class Menu {
 
     private static List<Object> myTeam = new ArrayList<>();
     private static List<Object> enemyTeam = new ArrayList<>();
     private static List<Object> graveyard = new ArrayList<>();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         playWarriorsVsWizards();
     }
 
-    public static void playWarriorsVsWizards() {
+    public static void playWarriorsVsWizards() throws InterruptedException {
         System.out.println(
-                        "##################################################################\n" +
+                "##################################################################\n" +
                         "#                                                                #\n" +
                         "#                      WARRIORS VS WIZARDS                       #\n" +
                         "#                                                                #\n" +
@@ -38,7 +39,7 @@ public class Menu {
         System.out.println("What a lovely name! We are happy to have you here, " + name + ".");
     }
 
-    public static void createTeam() {
+    public static void createTeam() throws InterruptedException {
         System.out.println("\n" + "Please choose an option. \n" +
                 "Press 1 to handpick the champions for your party. \n" +
                 "Press 2 to let the local champions form their own party. \n" +
@@ -53,9 +54,9 @@ public class Menu {
             case "2":
                 createTeamOption2(); // Both teams are created randomly
                 break;
-            case "3":
-                createTeamOption3(); // Import your own team, enemy team randomly created
-                break;
+//            case "3":
+//                createTeamOption3(); // Import your own team, enemy team randomly created
+//                break;
             case "q":
                 quitGame(); // Quit and start a new game from the beginning
                 break;
@@ -70,44 +71,57 @@ public class Menu {
     }
 
     // Method for option 1: Add and customize each member of your team individually, enemy team randomly created
-    public static void createTeamOption1() {
-        //myTeam = createParty();
+    public static void createTeamOption1() throws InterruptedException {
+        Fight fight = new Fight();
+        InputOutput io = new InputOutput();
+        myTeam = io.createParty();
+        //System.out.println(myTeam); // Remove?
 
         // Option to export your team
         askExport(myTeam);
 
         // Create the enemy team randomly
         int teamsize = getTeamsize(myTeam);
-        //enemyTeam = createRandomParty(teamsize);
+        enemyTeam = fight.createRandomParty(teamsize);
         System.out.println("The teams are set and ready to fight! Each team has " + teamsize + " team member(s).");
     }
 
     // Method for option 2: Both teams are created randomly
     public static void createTeamOption2() {
+        Fight fight = new Fight();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Let's tell the champions to form their own party. How many team members should each team have?");
-        int teamsize = scanner.nextInt();
-        //myTeam = createRandomParty(teamsize);
-        //enemyTeam = createRandomParty(teamsize);
-        System.out.println("The teams are set and ready to fight! Each team has " + teamsize + " team member(s).");
+        while (true) {
+            int teamsize = scanner.nextInt();
+            if (teamsize > 0) {
+                myTeam = fight.createRandomParty(teamsize);
+                enemyTeam = fight.createRandomParty(teamsize);
+                System.out.println("The teams are set and ready to fight! Each team has " + teamsize + " team member(s).");
+                break;
+            } else {
+                System.out.println("The team size cannot be 0 or less. Please try again.");
+            }
+        }
     }
 
     // Method for option 3: Import your own team, enemy team randomly created
-    public static void createTeamOption3() {
-        //myTeam = readFile(aFile);
-        //int teamsize = getTeamsize(myTeam);
-        //enemyTeam = createRandomParty(teamsize);
-        //System.out.println("The teams are set and ready to fight! Each team has " + teamsize + " team member(s).");
-    }
+//    public static void createTeamOption3() {
+//        InputOutput io = new InputOutput();
+//        myTeam = io.readFile(aFile);
+//        int teamsize = getTeamsize(myTeam);
+//        enemyTeam = Fight.createRandomParty(teamsize);
+//        System.out.println("The teams are set and ready to fight! Each team has " + teamsize + " team member(s).");
+//    }
 
-    public static void askExport(List<Object> myList) {
+    public static void askExport(List<Object> myList) throws InterruptedException {
+        InputOutput io = new InputOutput();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nice team! Do you want to export it in order to be able to use it again in the next game? \n" +
                 "Press 1 for YES and 2 for NO. If you want to quit the game to start a new one, press q.");
         String answer = scanner.nextLine();
         switch (answer) {
             case "1":
-                //exportParty(myTeam1);
+                io.exportParty(myTeam);
                 break;
             case "2":
                 break;
@@ -120,18 +134,18 @@ public class Menu {
         }
     }
 
-    public static void quitGame() {
+    public static void quitGame() throws InterruptedException {
         myTeam.clear();
         enemyTeam.clear();
         playWarriorsVsWizards();
     }
 
-    public static void chooseGameMode() {
+    public static void chooseGameMode() throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("One last question. How would you like play? \n" +
-                            "Press 1 to select the champion for each round. \n" +
-                            "Press 2 to watch the battle unfold. \n" +
-                            "Press q to quit.");
+                "Press 1 to select the champion for each round. \n" +
+                "Press 2 to watch the battle unfold. \n" +
+                "Press q to quit.");
         switch(scanner.nextLine()) {
             case "1":
                 //Method to instantiate outer loop
@@ -152,43 +166,54 @@ public class Menu {
         }
     }
 
-    public static void playNormalBattle() {
+    public static void playNormalBattle() throws InterruptedException {
+        Fight fight = new Fight(); //changed: to use non-static Fight class methods
         while(hasPartyFighters()) {
-            // selectFighter(); Anna's method
-            // Fight / attack methods?
+            Character champion = (Character) fight.selectFighter(myTeam);
+            Character opponent = (Character) fight.randomFighters(enemyTeam);
+            fight.fightToDeath(champion, opponent);
+            fight.checkFighterHp(champion, myTeam);
+            fight.checkFighterHp(opponent, enemyTeam);
         }
+        graveyard = Fight.getGraveyard();
         evaluateGame();
     }
 
-    public static void playWithOneClick() {
+    public static void playWithOneClick() throws InterruptedException {
+        Fight fight = new Fight();
         while(hasPartyFighters()) {
-            // randomFighters(); Anna's method
-            // Fight / attack methods?
+            Character champion = (Character) fight.randomFighters(myTeam);
+            Character opponent = (Character) fight.randomFighters(enemyTeam);
+            fight.fightToDeath(champion, opponent);
+            fight.checkFighterHp(champion, myTeam);
+            fight.checkFighterHp(opponent, enemyTeam);
         }
+        graveyard = Fight.getGraveyard();
         evaluateGame();
     }
 
     public static boolean hasPartyFighters() {
-        if(myTeam.size() == 0 || enemyTeam.size() == 0) {
+        if (myTeam.size() == 0 || enemyTeam.size() == 0) {
             return false;
         }
         return true;
     }
 
-    public static void evaluateGame() {
+    public static void evaluateGame() throws InterruptedException {
+        Fight fight = new Fight();
+
         if(myTeam.size() == 0) {
             System.out.println("Your team has lost the tournament. \n" +
-                            "These are the fighters that survived until the end: " + enemyTeam.toString());
+                    "These are the fighters that survived until the end: " + fight.toString(enemyTeam));
         } else {
             System.out.println("Congratulations! Your team has won! \n" +
-                                "These are the fighters that survived until the end: " + myTeam.toString());
+                    "These are the fighters that survived until the end: " + fight.toString(myTeam));
         }
 
-        System.out.println("These are the fighters that succumbed their wounds: " + graveyard.toString());
-
+        System.out.println("These are the fighters that succumbed their wounds: " + fight.toString(graveyard));
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("If you want to start a new game, please press q.");
+        System.out.println("If you want to start a new game, please press q. Press Enter to end your adventure.");
         String newGame = scanner.nextLine();
 
         if(newGame.equals("q")) {
@@ -196,5 +221,4 @@ public class Menu {
         }
         scanner.close();
     }
-
 }
