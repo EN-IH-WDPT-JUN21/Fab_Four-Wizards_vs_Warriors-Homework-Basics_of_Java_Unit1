033341fc29c1cecfd55f50cdafe7c5c9a45b23df
wi3tek <patrykwieteskapw@gmail.com>
2021-07-06 15:15:07 +0200

    added executable file run.bat

--- src/test/java/com/ironhack/battlesimulator/common/InputOutputTest.java
+++ src/test/java/com/ironhack/battlesimulator/common/InputOutputTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputOutputTest {
@@ -18,8 +21,17 @@ class InputOutputTest {
 
     @Test
     @DisplayName("Test: readFile(). Asserts that List returned")
-    public void InputOutput_readFileTest_ObjectReturned() {
+    public void InputOutput_readFileTest_ObjectReturned() throws IOException {
+
+        File file = new File("BattleStar_2021-07-03.csv");
+        file.createNewFile();
+        File file1 = new File("Merry_2021-07-04.csv");
+        file1.createNewFile();
+
         assertNotNull(tester.readFile("BattleStar_2021-07-03.csv"));
         assertNotNull(tester.readFile("Merry_2021-07-04.csv"));
+
+        file.delete();
+        file1.delete();
     }
 }
